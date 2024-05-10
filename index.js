9e0905bef0c7c60b34d4593f18af5a7c1fff706d
drtfyguh7 <js1leon6546@hotmail.com>
2024-05-10 20:18:04 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const fruits = ["apple", "banana", "orange"];
-for (const fruit of fruits) {
-  console.log(fruit);
+function pathSum(root, sum) {
+  if (!root) return [];
+  const result = [];
+  const path = [];
+  const traverse = (node, sum) => {
+    if (!node) return;
+    path.push(node.val);
+    if (!node.left && !node.right && sum === node.val) {
+      result.push([...path]);
+    }
+    traverse(node.left, sum - node.val);
+    traverse(node.right, sum - node.val);
+    path.pop();
+  };
+  traverse(root, sum);
+  return result;
 }
